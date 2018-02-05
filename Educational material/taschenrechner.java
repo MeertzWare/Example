abbf4521969fc7b15261bbea940cf683cb515e87
bmeertz <36151461+bmeertz@users.noreply.github.com>
2018-02-05 10:04:49 +0100

    Add files via upload

---
+++ Educational material/taschenrechner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Taschenrechner {
+
+	public static void main(string[] args) {
+		double ziffer1, ziffer2, loesung;
+		Scanner scannerVariable = new Scanner(system.in);
+		
+		System.out.printIn("1. Zahl eingeben");
+		ziffer1=scnnervariable.newxtDouble();
+		System.out.printIn(2. Zahl eingeben");
+		ziffer2=scannerVariable.nextDouble();
+		loesung=ziffer1+ziffer2;
+		System.out.printIn("Die Lösung der Adition ist: " + loesung);
+	}
+}
